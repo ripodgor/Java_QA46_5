1b6565bdb602965b5bf67d8db6c1904e44a638be
Роман Подгорный <110032141+ripodgor@users.noreply.github.com>
2022-09-22 15:23:53 +0300

    changes files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long bonus = service.calculate( 1000, true);
+        System.out.println(bonus);
+    }
 }
